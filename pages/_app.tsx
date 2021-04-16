33ec3da66ebee920cd55013800c5fb58ba485b2b
Bem Hur <bemhur.ganem@gmail.com>
2021-04-16 18:08:34 -0300

    bootstrap, firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/global.css'
 import { AppProps } from 'next/app'
 
---
+++ utils/firestore-util.ts
@@ -0,0 +1,8 @@
+import 'firebase/storage'
+import fire from './firebase-util';
+
+
+const firestore = fire.firestore();
+
+// export default firestore;
+export default firestore;
