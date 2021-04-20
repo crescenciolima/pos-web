661d7ad76da080913498451f83fd62f0abb876a3
Bem Hur <bemhur.ganem@gmail.com>
2021-04-20 07:31:52 -0300

    Correção da variável de ambiente NEXT_PUBLIC_VERCEL_URL

--- lib/api.routes.ts
+++ lib/api.routes.ts
@@ -1,6 +1,6 @@
 export class APIRoutes {
 
-    static API_URL = process.env.VERCEL_URL;
+    static API_URL = process.env.NEXT_PUBLIC_VERCEL_URL;
 
     static DOCENTES:string = APIRoutes.API_URL + "/api/docente"
 
