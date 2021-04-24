d6ba35ebf308e171eaeaa86ebfb313c1f950f82f
Leticia <leticiaportosoares@gmail.com>
2021-04-24 10:00:05 -0300

    incluindo cors

--- pages/api/course.ts
+++ pages/api/course.ts
@@ -1,10 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import CourseService from '../../lib/course.service';
 import { Course } from '../../models/course';
+import Cors from 'cors'
+import initMiddleware from '../../lib/init-middleware'
+
+const cors = initMiddleware(
+    // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+    Cors({
+        // Only allow requests with GET, POST and OPTIONS
+        methods: ['GET', 'POST', 'OPTIONS'],
+    })
+)
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     //Create a firestore course service
     const courseService = CourseService();
+
+    await cors(req, res);
 
     if (req.method === 'POST') {
         // Process a POST request
